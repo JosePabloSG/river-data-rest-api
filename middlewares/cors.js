beda4cdf2a14d5bfa0860e3ab207a5a350c96dad
José Pablo Suárez Gómez <suarezgomezjosepablo03@gmail.com>
2023-09-30 17:04:42 -0600

    add localhost

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,8 @@ import cors from 'cors'
 const ACCEPTED_ORIGINS = [
   'http://localhost:8080',
   'http://localhost:3000',
-  'http://localhost:5500'
+  'http://localhost:5500',
+  'http://127.0.0.1:5500'
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
