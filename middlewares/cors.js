3aa6f8abce4dfc89d427f39457efd064b12ffceb
José Pablo Suárez Gómez <suarezgomezjosepablo03@gmail.com>
2023-10-01 20:09:29 -0600

    add deployings

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,13 +1,14 @@
 import cors from 'cors'
 
 const ACCEPTED_ORIGINS = [
+  // Localhost
   'http://localhost:8080',
   'http://localhost:3000',
   'http://localhost:5500',
   'http://127.0.0.1:5500',
-  'https://joseabh.github.io/AppAlertaRio',
-  'https://joseabh.github.io/AppAlertaRio/historial.html',
-  'https://joseabh.github.io'
+  // Despliegues
+  'https://joseabh.github.io',
+  'https://back-pzj5-dev.fl0.io'
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
