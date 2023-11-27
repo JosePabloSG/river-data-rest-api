0532efc3871afeecec70ff98a516e5e6ac78ff3a
JosePabloSG <suarezgomezjosepablo03@gmail.com>
2023-11-26 20:29:04 -0600

    Remove unnecessary code and comments

--- app.js
+++ app.js
@@ -8,13 +8,10 @@ export const createApp = ({ dataModel }) => {
   app.use(json())
   app.use(corsMiddleware())
   app.disable('x-powered-by')
-
   app.use('/data', createDataRouter({ dataModel }))
-
   // arranque del servidor
   const PORT = process.env.PORT ?? 3000
   app.listen(PORT, () => {
     console.log(`Listening on Port http://localhost:${PORT}`)
-    //test
   })
 }
